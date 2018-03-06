6fdc5d659b545aba921529a87df8e2a19ae1e529
Radislaw <radislaw22@gmail.com>
2018-03-06 19:56:05 +1000

    start brain-even

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import readlineSync from 'readline-sync';
+import getRandomNum from './helpers/randomNumber';
 
-export default () => {
+
+export const greetUser = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello ${userName}!`);
 };
+
+
+export const evensGame = () => {
+  const randomNum = getRandomNum();
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!`);
+  const answer = readlineSync.question(`Question: ${randomNum} `, {
+    trueValue: ['yes'],
+    falseValue: ['no'],
+  });
+  const isEven = randomNum % 2 === 0;
+  if (isEven === answer) {
+    console.log('Correct!');
+  }
+  console.log(randomNum);
+  // сгенерировать число и вывести его на экран
+  // определить ответ
+  //  если число четное - да, иначе нет
+  // если ответ неправильный или введен неверный символ(кроме yes, no)
+  //  вывести сообщение 'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, Bill!
+  //  завершить игру
+  // если ответ правильный отобразить 'Correct!'
+  //  продолжить игру 3 раза
+  // после завершения игры вывести Congratulations, Bill!
+};
+
