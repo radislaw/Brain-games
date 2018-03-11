a52b3ac56be84276365dc83a29c0347ed56eb0d6
Radislaw <radislaw22@gmail.com>
2018-03-11 10:48:43 +1000

    add colors to output using chalk

--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
 import readlineSync from 'readline-sync';
 import { car, cdr } from 'hexlet-pairs';
+import chalk from 'chalk';
 
 const GAME_COUNT = 3;
+const greetingColor = '#00BCD4';
+const missionColor = '#26A69A';
+const questionColor = '#448AFF';
+const successColor = '#00E676';
+const errorColor = '#FF1744';
 
 export const greetUser = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!\n`);
+  const userName = readlineSync.question(chalk.hex(questionColor).bold('May I have your name? '));
+  console.log(chalk.hex(greetingColor).bold(`Hello ${userName}!`));
   return userName;
 };
 
 export default (mission, getQuestionAndAnswer) => {
-  console.log('Welcome to the Brain Games!\n', mission);
+  console.log(chalk.hex(greetingColor).bold('Welcome to the Brain Games!'));
+  console.log(chalk.hex(missionColor).bold(mission));
   const userName = greetUser();
   const checkAnswer = (times = GAME_COUNT) => {
     if (!times) {
-      console.log(`Congratulations, ${userName}!`);
+      console.log(chalk.hex(successColor).bold(`Congratulations, ${userName}!`));
       return;
     }
     const questionAndAnswer = getQuestionAndAnswer();
-    console.log(`Question: ${car(questionAndAnswer)}`);
+    console.log(chalk.hex(questionColor).bold(`Question: ${car(questionAndAnswer)}`));
     const correctAnswer = cdr(questionAndAnswer);
-    const answer = readlineSync.question('Your answer: ');
+    const answer = readlineSync.question(chalk.hex(questionColor).bold('Your answer: '));
     if (`${correctAnswer}` !== answer) {
-      console.log(`'${answer}' is wrong answer. \n Correct answer was '${correctAnswer}'. Let's try again, ${userName}!`);
+      console.log(chalk.hex(errorColor)
+        .bold(`'${answer}' is wrong answer. \nCorrect answer was '${correctAnswer}'. Let's try again, ${userName}!`));
     } else {
-      console.log('Correct!');
+      console.log(chalk.hex(successColor).bold('Correct!'));
       checkAnswer(times - 1);
     }
   };
