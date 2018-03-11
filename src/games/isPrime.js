498784aa19b4fcbbfa666ffc395ebfc2635e1a40
Radislaw <radislaw22@gmail.com>
2018-03-11 22:01:49 +1000

    change answer to predicat

--- src/games/isPrime.js
+++ src/games/isPrime.js
@@ -3,20 +3,20 @@ import makeGame from '..';
 import getRandomNum from '../helpers/randomNumber';
 
 export default () => {
-  const mission = 'Answer "yes" if number is prime otherwise answer "no".';
+  const mission = 'Answer "true" if number is prime otherwise answer "false".';
   const getQuestionAndAnswer = () => {
     const randomNum = getRandomNum();
     const question = `Is this number prime? ${randomNum}`;
     const correctAnswer = (n) => {
       if (n < 2) {
-        return 'no';
+        return false;
       }
       for (let i = 2; i <= n / 2; i += 1) {
         if (n % i === 0) {
-          return 'no';
+          return true;
         }
       }
-      return 'yes';
+      return false;
     };
     return cons(question, correctAnswer(randomNum));
   };
